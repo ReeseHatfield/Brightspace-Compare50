b6894646ade8e14c5eec70c6877cf6f6a3c7912b
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-01-09 18:40:48 -0500

    add install command & play around with imports

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Brightspace-Compare50
+
+git+https://github.com/OfficeCoffee/GradingFileProject.git
--- d2l-cs50/__main__.py
+++ d2l-cs50/__main__.py
@@ -1,2 +1,7 @@
+import compare50
+import grading-script
+
 if __name__ == "__main__":
     print("Hello world!")
+
+    compare50.comparators.Winnowing()
